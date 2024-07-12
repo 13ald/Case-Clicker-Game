33e0670394f329b8bedd4e28069fc7db9ed3812a
13ald <shanefinlayson1@gmail.com>
2024-07-12 16:37:53 +0100

    Second Commit

--- clicker.js
+++ clicker.js
@@ -1,11 +1,10 @@
 let cashCount = 0;
 let multiplier = 0;
+let perTap = 1;
 
 function addMoney() {
-    cashCount = cashCount + 1;
+    cashCount = cashCount + perTap;
     updateMoneyCount(cashCount);
-    console.log(cashCount);
-    
 }
 
 function getMoneyCount() {
@@ -20,11 +19,26 @@ function saveMoneyCount(money) {
 function onLoad() {
     cashCount = getMoneyCount();
     updateMoneyCount(cashCount);
+    updateTapPerSec(tapVal)
 }
 
 function updateMoneyCount(money) {
     document.getElementById("moneyCounter").innerHTML = money;
     saveMoneyCount(money);
+}
+
+function getTapVal(){
+    let localTapPerSec = window.localStorage.getItem("perTap");
+    return Number.parseInt(localTapPersec)
+}
+function savePerSecCount(tapVal) {
+    window.localStorage.setItem("perTap", tapVal);
+}
+
+function updateTapPerSec(tapVal){
+    document.getElementById("moneyPerSec") = tapVal;
+    saveTapVal(tapVal);
+
 }
 
 onLoad();
