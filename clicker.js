37099f0f04b204db8f51b84523ea8c747d2aa123
13ald <shanefinlayson1@gmail.com>
2024-07-13 15:14:16 +0100

    Edits to json & JS

--- clicker.js
+++ clicker.js
@@ -1,6 +1,7 @@
 let cashCount = 0;
 let multiplier = 0;
 let perTap = 1;
+let caseData = {};
 
 function addMoney() {
     cashCount = cashCount + perTap;
@@ -9,6 +10,9 @@ function addMoney() {
 
 function getMoneyCount() {
     let localCashCount = window.localStorage.getItem("cashCount");
+    if (!localCashCount) {
+        localCashCount = "0";
+    }
     return Number.parseInt(localCashCount);
 }
 
@@ -17,10 +21,11 @@ function saveMoneyCount(money) {
 }
 
 function onLoad() {
+    getCaseData();
     cashCount = getMoneyCount();
     tapVal = 1;
     updateMoneyCount(cashCount);
-    updateTapPerSec(tapVal)
+    updateTapPerSec(tapVal);
 }
 
 function updateMoneyCount(money) {
@@ -28,22 +33,95 @@ function updateMoneyCount(money) {
     saveMoneyCount(money);
 }
 
-function getTapVal(){
+function getTapVal() {
     let localTapPerSec = window.localStorage.getItem("perTap");
-    return Number.parseInt(localTapPerSec)
+    return Number.parseInt(localTapPerSec);
 }
 function savePerSecCount(tapVal) {
     window.localStorage.setItem("perTap", tapVal);
 }
 
-function updateTapPerSec(tapVal){
+function updateTapPerSec(tapVal) {
     document.getElementById("moneyMulti").innerHTML = tapVal;
     savePerSecCount(tapVal);
-
 }
-function addPerSec(){
+function addPerSec() {
     perTap = perTap * 1.1;
-    updateTapPerSec(tapVal)
+    updateTapPerSec(tapVal);
+}
+
+function buyItem(item) {
+    const caseData = {};
+
+    const itemToBuy = caseData[item];
+    return itemToBuy;
+}
+
+function getCaseData() {
+    fetch("./cases.json")
+        .then((res) => res.json())
+        .then((cases) => {
+            caseData = cases;
+            console.log("Successfully read case data");
+
+            const shopItemsRef = document.getElementById("shopItems");
+            if (!shopItemsRef) return;
+
+            const keys = Object.keys(cases);
+
+            /**
+             * <div class="shop-item" id="<CASE_KEY>">
+                    <img
+                        class="shop-item-image"
+                        src=".\Assets\Case Images\CS_GO_Weapon_Case.png" />
+
+                    <p class="shop-text">CS:GO Weapon Case</p>
+                    <label>BUY $90</label>
+                    <label><span id="wpnCaseOwned"></span> Cases Owned</label>
+                </div>
+ */
+
+            for (const key of keys) {
+                const caseData = cases[key];
+                console.log(caseData);
+
+                // Create Parent Element
+                const shopItem = document.createElement("div");
+                shopItem.classList.add("shop-item");
+                shopItem.id = key;
+
+                // Create Image Element
+                const img = document.createElement("img");
+                img.src = "./Assets/Case Images/" + caseData.img; // Modify attribute
+                img.classList.add("shop-item-image"); // Add class to element
+                shopItem.appendChild(img); // Append element to parent
+
+                // Create Shop Text
+                const p = document.createElement("p");
+                p.textContent = caseData.displayName;
+                p.classList.add("shop-text");
+                shopItem.appendChild(p);
+
+                // Create Price Tag
+                const label = document.createElement("label");
+                label.textContent = "$" + caseData.price;
+                shopItem.appendChild(label);
+
+                // Create Owned
+                const labelOwned = document.createElement("label");
+                labelOwned.textContent = "Cases Owned";
+                shopItem.appendChild(labelOwned);
+
+                const spanOwned = document.createElement("span");
+                spanOwned.id = key + "Owned";
+                labelOwned.appendChild(spanOwned);
+
+                document.getElementById("shopItems").appendChild(shopItem);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 }
 
 onLoad();
